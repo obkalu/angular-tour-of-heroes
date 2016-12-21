d26f2df54e2feea79f91f6d76bfb26dabe52f7ea
Obinna A. Kalu <obkalu@gmail.com>
2016-12-21 11:13:26 -0600

    Added fetching remote data (simulated) using Http client

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent }  from './app.component';
 import { HeroService } from './hero.service';
@@ -10,13 +11,23 @@ import { HeroDetailComponent } from './hero-detail.component';
 
 import { AppRoutingModule }     from './app-routing.module';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AppRoutingModule ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule, 
+    AppRoutingModule, 
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService), 
+    ],
   declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent ],
   providers: [ HeroService],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { 
-  
+  //
 }
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,19 +1,35 @@
 import { Injectable } from '@angular/core';
 
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+//import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
 
+    private heroesUrl: string = 'api/heroes';  // URL to web api
+
+    constructor(private http: Http) { }
+
     getHeroes(): Promise<Hero[]> {
-        return Promise.resolve(HEROES);
+        return this.http.get(this.heroesUrl)
+                .toPromise()
+                .then(response => response.json().data as Hero[])
+                .catch(this.handleError);
     }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
+    }    
 
     getHeroesSlowly(): Promise<Hero[]> {
     return new Promise(resolve => {
         // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(this.getHeroes()), 3000);
+        setTimeout(() => resolve(this.getHeroes()), 2000);
         });
     }
 
