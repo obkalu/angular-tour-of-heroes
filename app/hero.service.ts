ccbc40e8ee46da3e58e0c0f4fa47bb7cdab70995
Obinna A. Kalu <obkalu@gmail.com>
2016-12-21 12:17:00 -0600

    Added functionalities for Save, Update and Delete

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -11,6 +11,7 @@ import { Hero } from './hero';
 export class HeroService {
 
     private heroesUrl: string = 'api/heroes';  // URL to web api
+    private headers = new Headers({'Content-Type': 'application/json'});
 
     constructor(private http: Http) { }
 
@@ -22,18 +23,48 @@ export class HeroService {
     }
 
     private handleError(error: any): Promise<any> {
-        console.error('An error occurred', error); // for demo purposes only
+        console.error('Oops! An error occurred', error); // for demo purposes only
         return Promise.reject(error.message || error);
     }    
 
     getHeroesSlowly(): Promise<Hero[]> {
     return new Promise(resolve => {
-        // Simulate server latency with 2 second delay
+        // Simulate server latency with a 2 second delay
         setTimeout(() => resolve(this.getHeroes()), 2000);
         });
     }
 
     getHero(id: number): Promise<Hero> {
-        return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get(url)
+        .toPromise()
+        .then(response => response.json().data as Hero)
+        .catch(this.handleError);
     }
+
+    update(hero: Hero): Promise<Hero> {
+    const url = `${this.heroesUrl}/${hero.id}`;
+    return this.http
+        .put(url, JSON.stringify(hero), {headers: this.headers})
+        .toPromise()
+        .then(() => hero)
+        .catch(this.handleError);
+    }    
+
+    create(name: string): Promise<Hero> {
+    return this.http
+        .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+        .toPromise()
+        .then(res => res.json().data)
+        .catch(this.handleError);
+    }
+
+    delete(id: number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete(url, {headers: this.headers})
+        .toPromise()
+        .then(() => null)
+        .catch(this.handleError);
+    }
+    
 }
