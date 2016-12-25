6e9f003876dd18c5c6059f2d19882b37adbce158
Obinna A. Kalu <obkalu@gmail.com>
2016-12-25 14:17:13 -0600

    Added search functionality; uses Observables from RxJS

--- app/app.module.ts
+++ app/app.module.ts
@@ -8,6 +8,7 @@ import { HeroService } from './hero.service';
 import { DashboardComponent } from './dashboard.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroSearchComponent } from './hero-search.component';
 
 import { AppRoutingModule }     from './app-routing.module';
 
@@ -15,6 +16,8 @@ import { AppRoutingModule }     from './app-routing.module';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
 
+// Import RxJS extensions
+import './rxjs-extensions';
 
 @NgModule({
   imports:      [ 
@@ -24,7 +27,13 @@ import { InMemoryDataService }  from './in-memory-data.service';
     HttpModule,
     InMemoryWebApiModule.forRoot(InMemoryDataService), 
     ],
-  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent ],
+  declarations: [ 
+    AppComponent, 
+    HeroDetailComponent, 
+    HeroesComponent, 
+    DashboardComponent, 
+    HeroSearchComponent
+  ],
   providers: [ HeroService],
   bootstrap:    [ AppComponent ]
 })
